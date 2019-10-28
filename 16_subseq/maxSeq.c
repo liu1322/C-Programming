0ee2fb66d96ebebb8683a6170e2291813618a3c3
Coursera Learner <nobody@nowhere.nul>
2019-10-28 00:28:25 +0000

    max seq

---
+++ 16_subseq/maxSeq.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+size_t maxSeq(int * array, size_t n){
+  if (n == 0){
+    return -1;
+  }
+  int max = 1;
+  int counter = 1;
+  for (int i; i < n-1; i++){
+    if (array[i] < array[i+1]){
+      counter += 1;
+    }
+    else{
+      max = counter;
+      counter = 1;
+    }
+  }
+  return max;
+}
