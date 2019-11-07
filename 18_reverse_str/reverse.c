d0bd6d6326af28bf248eb64e60e5675ecdc59b24
Coursera Learner <nobody@nowhere.nul>
2019-11-07 05:00:02 +0000

    a corner case

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -3,6 +3,9 @@
 #include <string.h>
 
 void reverse(char * str) {
+  if (!str){
+    return;
+  }
   char * p1 = str;
   if (*p1 == '\0'){
     return;
@@ -29,8 +32,8 @@ int main(void) {
   char str4[] = "Hello, my name is Inigo Montoya.";
   char str5[] = "You can be my wingman anyday!";
   char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
-  char * array[] = {str0, str1, str2, str3, str4, str5, str6};
-  for (int i = 0; i < 7; i++) {
+    char * array[] = {str0, str1, str2, str3, str4, str5, str6};
+    for (int i = 0; i < 7; i++) {
     reverse(array[i]);
     printf("%s\n", array[i]);
   }
