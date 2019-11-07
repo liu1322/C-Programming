c11390cb50ca19d63441ed02fa5e3ddc5967ae50
Coursera Learner <nobody@nowhere.nul>
2019-11-07 07:30:01 +0000

    matrix rotation

---
+++ 20_rot_matrix/rotate.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void rotate(char matrix[10][10]){
+  for (int i = 0; i < 5; i++){
+      for (int j = i; j < 10 - i - 1; j++){
+	  int temp = matrix[i][j];
+	  matrix[i][j] = matrix[10 - 1 - j][i];
+	  matrix[10 - 1 - j][i] = matrix[10 - 1 - i][10 - 1 - j];
+	  matrix[10 - 1 - i][10 - 1 - j] = matrix[j][10 - 1 - i];
+	  matrix[j][10 - 1 - i] = temp;
+	}
+    }
+}
